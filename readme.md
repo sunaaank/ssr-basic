46fb0d8d47295a0b25f6b9c843939c94c5adce34
sunaaank <suna.namgung@gmail.com>
2021-07-25 15:21:07 +0900

    feat: use renderToNodeStream, stream rendering caching SSR without Next.js

--- readme.md
+++ readme.md
@@ -1,6 +1,12 @@
-# 서버사이드렌더링(SSR) 테스트용 레파지토리
+# 서버사이드렌더링(SSR) 학습용 레파지토리
 
-- 책 <실전 리액트 프로그래밍(이재승)>의 8장 `서버사이드 렌더링 그리고 Next.js`를 참고했습니다.
+- 책 <실전 리액트 프로그래밍(이재승)>의 `8장 서버사이드 렌더링 그리고 Next.js`를 참고했습니다.
+- 본 레파지토리는 SSR 관련 프로젝트 두 개를 관리하고 있습니다.
+
+1. ssr-with-nextjs: nextjs를 적용해 ssr를 구현한 프로젝트
+   1-1. 실행 명령어:
+2. ssr-without-nextjs: nextjs 없이 기본적인 ssr을 구현한 프로젝트
+   2-1. 실행 명령어: `npm install` -> `npm run build` -> `npm start`
 
 ## 🎯 학습목표
 
@@ -9,7 +15,9 @@
 - styled-components로 작성된 스타일이 SSR 시 어떻게 처리되는지 알아본다
 - 서버용 번들 파일을 만드는 방법을 알아본다.
 
-## 사용 패키지
+## 🎁 사용 패키지
+
+### ssr-without-nextjs
 
 1. React
 
@@ -28,3 +36,15 @@
 - `express`: 웹 서버용
 - `@babel/cli`: 서버에서 사용될 JS파일을 컴파일할 때 사용(리액트 jsx문법으로 작성된 파일 변환)
 - `@babel/plugin-transform-modules-commonjs`: ESM으로 작성된 모듈시스템을 commonJS로 변경
+
+5. css-in-js
+
+- `styled-components`
+
+6. image module
+
+- `webpack-node-externals` `file-loader`
+
+7. caching
+
+- `lru-cache`: 정해진 최대 캐시 개수를 초과하면 LRU(Least Recently Used) 알고리즘에 따라 가장 오랫동안 사용되지 않은 캐시를 제거함
--- ssr-without-nextjs/src/server.js
+++ ssr-without-nextjs/src/server.js
@@ -1,7 +1,12 @@
 import express from "express";
 import LRUCache from "lru-cache";
+import { ServerStyleSheet } from "styled-components";
+import React from "react";
+import { renderToNodeStream } from "react-dom/server";
 import fs from "fs";
 import path from "path";
+import App from "./App";
+import { Transform } from "stream";
 import { renderPage, prerenderPages } from "./common";
 
 const ssrCache = new LRUCache({
@@ -9,9 +14,29 @@ const ssrCache = new LRUCache({
   max: 100,
   maxAge: 1000 * 60,
 });
+
+// 중간에 삽입할 스트림을 생성해주는 함수
+function createCacheStream(cacheKey, prefix, postfix) {
+  // 스트림으로 전달된 모든 청크 데이터를 저장하는 배열
+  const chunks = [];
+  // Transform: 읽기 쓰기가 모두 가능한 스트림 객체
+  return new Transform({
+    // chunk 데이터를 받으면 호출되는 함수. 전달받은 chunk 데이터를 그대로 chunks에 넣음
+    transform(data, _, callback) {
+      chunks.push(data);
+      callback(null, data);
+    },
+    // 청크 데이터가 모두 전달된 후 호출되는 함수. 완성된 HTML 데이터를 만들고 캐싱함
+    flush(callback) {
+      const data = [prefix, Buffer.concat(chunks).toString(), postfix];
+      ssrCache.set(cacheKey, data.join(""));
+      callback();
+    },
+  });
+}
+
 // 📧 미들웨어, url경로 설정 가능
 const app = express();
-
 // prerender.js 파일이 실행될 때 미리 렌더링 해놓은 페이지를 prerenderHtml 객체에 저장
 const prerenderHtml = {};
 for (const page of prerenderPages) {
@@ -21,6 +46,10 @@ for (const page of prerenderPages) {
   );
   prerenderHtml[page] = pageHtml;
 }
+
+const html = fs
+  .readFileSync(path.resolve(__dirname, "../dist/index.html"), "utf8")
+  .replace("__STYLE_FROM_SERVER__", "");
 
 // url이 dist로 시작하는 경우, dist 폴더 밑에 있는 정적 파일로 연결한다
 app.use("/dist", express.static("dist"));
@@ -50,16 +79,45 @@ app.get("*", (req, res) => {
   const initialData = { page };
 
   // 미리 렌더링된 페이지가 아닌 경우에만 새로 렌더링한다.
-  const pageHtml = prerenderPages.includes(page)
+
+  const isPrerender = prerenderPages.includes(page)
     ? prerenderHtml[page]
     : renderPage(page);
-  const result = pageHtml.replace(
+  const result = (isPrerender ? prerenderHtml[page] : html).replace(
     "__DATA_FROM_SERVER__",
     JSON.stringify(initialData)
   );
   // 💰 캐시가 존재하지 않으면 SSR 후 그 결과를 캐시에 저장한다
-  ssrCache.set(cacheKey, result);
-  res.send(result);
+  if (isPrerender) {
+    ssrCache.set(cacheKey, result);
+    res.send(result);
+  } else {
+    // root 요소를 기준으로 이전 문자열과 이후 문자열로 나눈다. 이전 문자열은 바로 전송한다.
+    const ROOT_TEXT = `<div id="root">`;
+    const prefix = result.substr(
+      0,
+      result.indexOf(ROOT_TEXT) + ROOT_TEXT.length
+    );
+    const postfix = result.substr(prefix.length);
+    res.write(prefix);
+    const sheet = new ServerStyleSheet();
+    const reactElement = sheet.collectStyles(<App page={page} />);
+    // renderToNodeStream 함수를 호출해서 읽기 가능한 스트림 객체를 만든다. 스트림 방식을 사용할 때는 interleaveWithNodeStream 메서드를 호출해야 한다. 이 메서드는 renderStream에서 스타일 코드가 생성되도록 하는 역할을 함
+    // 스타일 코드를 root 요소 내부에 삼입한다
+    const renderStream = sheet.interleaveWithNodeStream(
+      renderToNodeStream(reactElement)
+    );
+    // 생성한 스트림을 두 스트림 사이에 연결함.
+    // 청크 데이터는 renderStream -> cacheStream -> res 순으로 흐름
+    const cacheStream = createCacheStream(cacheKey, prefix, postfix);
+    cacheStream.pipe(res);
+    // pipe: renderStream 스트림과 res 스트림을 연결한다
+    // end:false 옵션은 스트림이 종료됐을 때 res.end 메서드가 자동으로 호출되지 않도록 함
+    renderStream.pipe(res, { end: false });
+    renderStream.on("end", () => {
+      res.end(postfix);
+    });
+  }
 });
 
 // 3000번 포트로 들어오는 클라이언트의 요청을 기다린다
