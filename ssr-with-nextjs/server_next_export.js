ccd9f942838741a47e401a9dc7e51802a90fa00e
sunaaank <suna.namgung@gmail.com>
2021-08-23 06:14:46 +0900

    feat:  페이지 미리 렌더링하기

---
+++ ssr-with-nextjs/server_next_export.js
@@ -0,0 +1,9 @@
+// next export로 단순 정적파일 서비스
+
+// const express = require("express");
+
+// const server = express();
+// server.use(express.static("out"));
+// server.listen(3000, err => {
+//   if (err) throw err;
+// });
